3f333e4b4e077f4af53d421214347a0367029900
Dhananjay Goratela <41978707+dhgoratela@users.noreply.github.com>
2021-05-01 15:06:01 +0530

    Basic logging

---
+++ python/logging/basic_logging_corey_ms.py
@@ -0,0 +1,39 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='test.log', format='%(asctime)s : %(levelname)s : %(message)s')
+
+
+def add(x, y):
+    """Add function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide function"""
+    return x / y
+
+
+num1 = 20
+num2 = 10
+
+add_result = add(num1, num2)
+logging.debug(f'Add: {num1} + {num2} = {add_result}')
+
+sub_result = subtract(num1, num2)
+logging.debug(f'Sub: {num1} - {num2} = {sub_result}')
+
+mul_result = multiply(num1, num2)
+logging.debug(f'Mul: {num1} * {num2} = {mul_result}')
+
+div_result = divide(num1, num2)
+logging.debug(f'Div: {num1} / {num2} = {div_result}')
